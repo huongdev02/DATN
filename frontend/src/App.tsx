93a9167b41da6a06539f1944e310b97fb4af2ff9
khoidev203 <khoidev203@gmail.com>
2024-09-23 23:50:05 +0700

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My name is Liên
+          My name is Liên 2024
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
