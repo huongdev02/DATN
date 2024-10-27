31f9d0141b7517bd01133d4ee13e378bb3f4794d
VT Giang <vtgiang2k4@gmail.com>
2024-10-27 22:27:36 +0700

    update bo loc orderuser

--- backend/app/Http/Controllers/UserOrderController.php
+++ backend/app/Http/Controllers/UserOrderController.php
@@ -9,7 +9,15 @@ class UserOrderController extends Controller
 {
     public function index(Request $request)
     {
-        $orders = Order::with('user', 'product', 'shipAddress')->paginate(5);
+        $query = Order::with(['product', 'user']); 
+    
+        if ($request->has('status')) {
+            $status = $request->get('status');
+            $query->where('status', $status);
+        }
+    
+        $orders = $query->latest()->paginate(5);
+    
         return view('user.order', compact('orders'));
     }
 }
