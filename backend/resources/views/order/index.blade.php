ba4a8acd0e91373389d5e52cb6db92bb44ace8e7
VT Giang <vtgiang2k4@gmail.com>
2024-10-29 15:16:40 +0700

    update view

--- backend/resources/views/order/index.blade.php
+++ backend/resources/views/order/index.blade.php
@@ -33,15 +33,16 @@
                             <form action="{{ route('orders.index') }}" method="GET" id="orderStatusForm-{{ $order->id }}">
                                 <input type="hidden" name="order_id" value="{{ $order->id }}">
                                 <select name="status" class="form-select" onchange="confirmAndSubmit(this)">
-                                    <option value="0" {{ $order->status == 0 ? 'selected' : '' }}>Chờ xử lý</option>
-                                    <option value="1" {{ $order->status == 1 ? 'selected' : '' }}>Đã xử lý</option>
-                                    <option value="2" {{ $order->status == 2 ? 'selected' : '' }}>Đang vận chuyển</option>
-                                    <option value="3" {{ $order->status == 3 ? 'selected' : '' }}>Giao hàng thành công</option>
-                                    <option value="4" {{ $order->status == 4 ? 'selected' : '' }}>Đã hủy</option>
-                                    <option value="5" {{ $order->status == 5 ? 'selected' : '' }}>Đã trả lại</option>
+                                    <option value="0" style="color: gray;" {{ $order->status == 0 ? 'selected' : '' }}>Chờ xử lý</option>
+                                    <option value="1" style="color: blue;" {{ $order->status == 1 ? 'selected' : '' }}>Đã xử lý</option>
+                                    <option value="2" style="color: orange;" {{ $order->status == 2 ? 'selected' : '' }}>Đang vận chuyển</option>
+                                    <option value="3" style="color: green;" {{ $order->status == 3 ? 'selected' : '' }}>Giao hàng thành công</option>
+                                    <option value="4" style="color: red;" {{ $order->status == 4 ? 'selected' : '' }}>Đã hủy</option>
+                                    <option value="5" style="color: purple;" {{ $order->status == 5 ? 'selected' : '' }}>Đã trả lại</option>
                                 </select>
                             </form>
                         </td>
+
                         <td>
                             <a href="{{ route('orders.show', $order->id) }}" class="btn btn-info btn-sm">Chi tiết</a>
                         </td>
