51ab1dd14245fb51e7bca5092a658e757ee4da0e
VT Giang <128453781+giangzzz1@users.noreply.github.com>
2024-12-14 00:36:58 +0700

    delete

--- backend/resources/views/user/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('user.master')
-
-@section('title')
-    Dashboard
-@endsection
-
-@section('content')
-    <a href="http://localhost:5001/" class="btn btn-primary mb-5"> Quay về trang chủ</a>
-@endsection
