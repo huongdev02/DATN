3041e55151f5471eebe438ad1420430eac03ec5f
nguyen1608 <nguyenntph31180@fpt.edu.vn>
2024-12-15 16:52:54 +0700

    done-blog

--- frontend/src/Component/Detail-Blog/BlogDetail.tsx
+++ frontend/src/Component/Detail-Blog/BlogDetail.tsx
@@ -1,16 +1,77 @@
 import api from "../../Axios/Axios";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./blog-detail.css";
 function BlogDetail() {
+  const [blog, setBlog] = useState<any>();
+  const { id } = useParams();
 
+  useEffect(() => {
+    const GetLogo = async () => {
+      try {
+        const { data } = await api.get(`/blog/${id}`);
+        setBlog(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    GetLogo();
+  }, []);
 
+  console.log("KKKK", blog);
 
+  const createdAt = blog?.created_at;
 
+  if (createdAt) {
+    const date = new Date(createdAt).toLocaleDateString("vi-VN", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
 
+    const time = new Date(createdAt).toLocaleTimeString("vi-VN", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    });
+    const formattedDateTime = `${date} - ${time}`;
+  }
 
-    
   return (
-    <section>
-      <h1>Blog detail</h1>
+    <section className="container-detail-blog">
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>
+          <p className="title-blog-detail">{blog?.title}</p>
+          <p style={{marginTop:'10px'}}>
+            {blog?.created_at &&
+              (() => {
+                const date = new Date(blog.created_at).toLocaleDateString(
+                  "vi-VN",
+                  {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  }
+                );
+                const time = new Date(blog.created_at).toLocaleTimeString(
+                  "vi-VN",
+                  {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                    hour12: false,
+                  }
+                );
+                return `${date} - ${time}`;
+              })()}
+          </p>
+          <hr className="hr-blog-detail" />
+          <p className="des-blog-detail">{blog?.description}</p>
+          <img className="img-blog-detail" src={blog?.image} alt="" />
+          <p className="content-blog-detail">{blog?.content}</p>
+        </div>
+      </div>
     </section>
   );
 }
