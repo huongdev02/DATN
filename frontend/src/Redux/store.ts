04e29dc9189b27ecb39f360071a2aaf3cb1a66b9
ducanh <ducdaph34986@fpt.edu.vn>
2024-12-18 00:56:58 +0700

    update

--- frontend/src/Redux/store.ts
+++ frontend/src/Redux/store.ts
@@ -11,7 +11,6 @@ import AuthReducer from './Reducer/AuthReducer'
 import VoucherReducer from './Reducer/VoucherReducer'
 import OrderHistoryReducer from './Reducer/OrderHistoryReducer'
 import UserReducer from './Reducer/UserReducer'
-import UserReducer from './Reducer/UserReducer'
 
 export const store = configureStore({
     reducer: {
